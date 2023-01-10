c00cd14cf08cee9260f9a0a644a2410caae4a425
Nikolay Karchevskiy <nikar.tom@gmail.com>
2023-01-10 22:08:21 +0700

    Transferring authorization to a separate file

---
+++ crm/auth_views.py
@@ -0,0 +1,80 @@
+from django.shortcuts import  render, redirect
+from django.http import HttpResponse
+from django.core.mail import send_mail, BadHeaderError
+
+from django.contrib import messages
+from django.contrib.auth import login, authenticate, logout, get_user_model
+from django.contrib.auth.forms import AuthenticationForm, PasswordResetForm
+from django.contrib.auth.tokens import default_token_generator
+
+from django.template.loader import render_to_string
+
+from django.db.models.query_utils import Q
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes
+
+from .forms import UserRegistrationForm
+
+def register_request(request):
+    if request.method == "POST":
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Регистрация прошла успешно!" )
+            return redirect("crm:homepage")
+        messages.error(request, "Регистрация не удалась. Проверьти правильность заполнения формы")
+    form = UserRegistrationForm()
+    return render (request=request, template_name="crm/register.html", context={"register_form":form})
+
+def login_request(request):
+	if request.method == "POST":
+		form = AuthenticationForm(request, data=request.POST)
+		if form.is_valid():
+			username = form.cleaned_data.get('username')
+			password = form.cleaned_data.get('password')
+			user = authenticate(username=username, password=password)
+			if user is not None:
+				login(request, user)
+				messages.info(request, f"Вы вошли как {username}.")
+				return redirect("crm:homepage")
+			else:
+				messages.error(request,"Неверное имя пользователя или пароль!")
+		else:
+			messages.error(request,"Неверное имя пользователя или пароль!")
+	form = AuthenticationForm()
+	return render(request=request, template_name="crm/login.html", context={"login_form":form})
+
+def logout_request(request):
+	logout(request)
+	messages.info(request, "Вы успешно вышли") 
+	return redirect("crm:homepage")
+
+def password_reset_request(request):
+	if request.method == "POST":
+		password_reset_form = PasswordResetForm(request.POST)
+		if password_reset_form.is_valid():
+			data = password_reset_form.cleaned_data['email']
+			User = get_user_model()
+			associated_users = User.objects.filter(Q(email=data))
+			if associated_users.exists():
+				for user in associated_users:
+					subject = "Запрос на сброс пароля"
+					email_template_name = "crm/password/password_reset_email.txt"
+					c = {
+					"email":user.email,
+					'domain':'127.0.0.1:8000',
+					'site_name': 'Website',
+					"uid": urlsafe_base64_encode(force_bytes(user.pk)),
+					"user": user,
+					'token': default_token_generator.make_token(user),
+					'protocol': 'http',
+					}
+					email = render_to_string(email_template_name, c)
+					try:
+						send_mail(subject, email, 'admin@example.com' , [user.email], fail_silently=False)
+					except BadHeaderError:
+						return HttpResponse('Invalid header found.')
+					return redirect ("/password_reset/done/")
+	password_reset_form = PasswordResetForm()
+	return render(request=request, template_name="crm/password/password_reset.html", context={"password_reset_form":password_reset_form})
--- crm/urls.py
+++ crm/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-from . import views
+from . import views, auth_views
 
 app_name = "crm"   
 
 
 urlpatterns = [
     path("", views.homepage, name="homepage"),
-    path("register", views.register_request, name="register"),
-    path("login", views.login_request, name="login"),
-    path("logout", views.logout_request, name= "logout"),
-    path("password_reset", views.password_reset_request, name="password_reset"),
+    path("register", auth_views.register_request, name="register"),
+    path("login", auth_views.login_request, name="login"),
+    path("logout", auth_views.logout_request, name= "logout"),
+    path("password_reset", auth_views.password_reset_request, name="password_reset"),
 ]
